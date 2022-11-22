fabf05ea6679a114f9c57f6c28f11000c88169e5
maxk1218 <66450526+maxk1218@users.noreply.github.com>
2022-11-21 19:57:45 -0600

    hola

--- app/Views/user/control/login.php
+++ app/Views/user/control/login.php
@@ -12,7 +12,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Login 1</title>
     
     <link rel="stylesheet" href="<?= base_url() ?>public/user/css/login.css ">
     
